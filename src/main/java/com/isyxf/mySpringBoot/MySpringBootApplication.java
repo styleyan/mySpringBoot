c4779b17bc108f056ac45dfcc5a1298f24bba0fa
yanxiaofei <forget_love2161@126.com>
2019-04-21 23:01:54 +0800

    feat: 初始化spring boot项目

---
+++ src/main/java/com/isyxf/mySpringBoot/MySpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.isyxf.mySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 说明这是一个SpringBoot应用的入口类
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        // 启动springBoot应用
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
